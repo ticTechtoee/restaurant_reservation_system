17a755e5c777cc3247f4c9192cefd1c0bcf6b082
Farhan Basheer <farhanfb6@outlook.com>
2022-09-30 22:46:15 +0500

    Fixed the Logout Issue

--- main/context_processors.py
+++ main/context_processors.py
@@ -1,7 +1,13 @@
 from .models import Menu
+from django.contrib.auth import login, authenticate,logout
+from django.shortcuts import  render, redirect
 
 def menu_navbar(request):
     menu_item = Menu.objects.all()
     return {
         'menu_item' : menu_item
     }
+
+def logout_request(request):
+	logout(request)
+	return redirect("login")
